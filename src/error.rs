8095880795592418cde260b263e19e31250333cb
Karl Smeltzer <github@karlsmeltzer.com>
2025-04-28 12:07:44 -0700

    scaffold for error recovery

--- src/error.rs
+++ src/error.rs
@@ -1,8 +1,22 @@
 use std::error;
+use std::fmt;
 
 pub type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
 
-pub enum Error {}
+#[derive(Debug)]
+pub enum Error {
+    ParseError,
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Error::ParseError => write!(f, "Parse error!"),
+        }
+    }
+}
+
+impl error::Error for Error {}
 
 pub fn report_error(line: usize, loc: String, message: String) {
     eprintln!("[line {}] Error {}: {}", line, loc, message);
--- src/parser.rs
+++ src/parser.rs
@@ -1,3 +1,4 @@
+use crate::error::{Error, Result, report_error};
 use crate::expr::{BinaryOp, Expr, Literal, UnaryOp, binop};
 use crate::lexer::{Token, TokenInfo};
 
@@ -19,9 +20,13 @@ impl Parser {
         self.tokens.get(self.current).map(|t| &t.token)
     }
 
-    fn consume_cont(&mut self, f: fn(&mut Self) -> Expr, cc: impl FnOnce(Expr) -> Expr) -> Expr {
+    fn consume_cont(
+        &mut self,
+        f: fn(&mut Self) -> Result<Expr>,
+        cc: impl FnOnce(Expr) -> Result<Expr>,
+    ) -> Result<Expr> {
         self.current += 1;
-        let rhs = f(self);
+        let rhs = f(self)?;
         cc(rhs)
     }
 
@@ -29,26 +34,42 @@ impl Parser {
         &mut self,
         lhs: Expr,
         op: BinaryOp,
-        f_rhs: fn(&mut Self) -> Expr,
-    ) -> Expr {
-        self.consume_cont(f_rhs, |rhs| binop(lhs, op, rhs))
+        f_rhs: fn(&mut Self) -> Result<Expr>,
+    ) -> Result<Expr> {
+        self.consume_cont(f_rhs, |rhs| Ok(binop(lhs, op, rhs)))
     }
 
-    fn consume_cont_unop(&mut self, op: UnaryOp, f_rhs: fn(&mut Self) -> Expr) -> Expr {
-        self.consume_cont(f_rhs, |rhs| Expr::Unary(op, Box::new(rhs)))
+    fn consume_cont_unop(
+        &mut self,
+        op: UnaryOp,
+        f_rhs: fn(&mut Self) -> Result<Expr>,
+    ) -> Result<Expr> {
+        self.consume_cont(f_rhs, |rhs| Ok(Expr::Unary(op, Box::new(rhs))))
     }
 
-    fn consume_literal(&mut self, lit: Literal) -> Expr {
+    fn consume_literal(&mut self, lit: Literal) -> Result<Expr> {
         self.current += 1;
-        Expr::Literal(lit)
+        Ok(Expr::Literal(lit))
+    }
+
+    fn synchronize(&mut self) {
+        // This isn't much better than doing nothing, just here to
+        // make sure the `Result` handling works as expected.
+        self.current += 1;
     }
 
     fn expr(&mut self) -> Expr {
-        self.equality()
+        match self.equality() {
+            Ok(e) => e,
+            Err(_) => {
+                self.synchronize();
+                self.expr()
+            }
+        }
     }
 
-    fn equality(&mut self) -> Expr {
-        let lhs = self.comparison();
+    fn equality(&mut self) -> Result<Expr> {
+        let lhs = self.comparison()?;
 
         match self.current_token() {
             Some(Token::EqualEqual) => {
@@ -57,12 +78,12 @@ impl Parser {
             Some(Token::BangEqual) => {
                 self.consume_cont_binop(lhs, BinaryOp::NotEqual, Parser::comparison)
             }
-            _ => return lhs,
+            _ => return Ok(lhs),
         }
     }
 
-    fn comparison(&mut self) -> Expr {
-        let lhs = self.term();
+    fn comparison(&mut self) -> Result<Expr> {
+        let lhs = self.term()?;
 
         match self.current_token() {
             Some(Token::Less) => self.consume_cont_binop(lhs, BinaryOp::LessThan, Parser::term),
@@ -73,31 +94,31 @@ impl Parser {
             Some(Token::GreaterEqual) => {
                 self.consume_cont_binop(lhs, BinaryOp::GreaterEqual, Parser::term)
             }
-            _ => return lhs,
+            _ => return Ok(lhs),
         }
     }
 
-    fn term(&mut self) -> Expr {
-        let lhs = self.factor();
+    fn term(&mut self) -> Result<Expr> {
+        let lhs = self.factor()?;
 
         match self.current_token() {
             Some(Token::Plus) => self.consume_cont_binop(lhs, BinaryOp::Plus, Parser::factor),
             Some(Token::Minus) => self.consume_cont_binop(lhs, BinaryOp::Minus, Parser::factor),
-            _ => return lhs,
+            _ => return Ok(lhs),
         }
     }
 
-    fn factor(&mut self) -> Expr {
-        let lhs = self.unary();
+    fn factor(&mut self) -> Result<Expr> {
+        let lhs = self.unary()?;
 
         match self.current_token() {
             Some(Token::Star) => self.consume_cont_binop(lhs, BinaryOp::Times, Parser::unary),
             Some(Token::Slash) => self.consume_cont_binop(lhs, BinaryOp::Div, Parser::unary),
-            _ => return lhs,
+            _ => return Ok(lhs),
         }
     }
 
-    fn unary(&mut self) -> Expr {
+    fn unary(&mut self) -> Result<Expr> {
         match self.current_token() {
             Some(Token::Bang) => self.consume_cont_unop(UnaryOp::Not, Parser::primary),
             Some(Token::Minus) => self.consume_cont_unop(UnaryOp::Negate, Parser::primary),
@@ -105,7 +126,7 @@ impl Parser {
         }
     }
 
-    fn primary(&mut self) -> Expr {
+    fn primary(&mut self) -> Result<Expr> {
         match self.current_token() {
             Some(Token::False) => self.consume_literal(Literal::False),
             Some(Token::True) => self.consume_literal(Literal::True),
@@ -123,17 +144,19 @@ impl Parser {
                 self.current += 1;
                 let expr = self.expr();
                 self.current += 1;
-                return Expr::Grouping(Box::new(expr));
+                Ok(Expr::Grouping(Box::new(expr)))
             }
             _ => self.parse_error(),
         }
     }
 
-    fn parse_error(&self) -> ! {
+    fn parse_error(&self) -> Result<Expr> {
         let token_info = self.tokens.get(self.current).unwrap();
-        panic!(
-            "Parse error on line {:?}: {:?}",
-            token_info.line, token_info.lexeme
+        report_error(
+            token_info.line,
+            token_info.lexeme.clone(),
+            "Parse error".to_string(),
         );
+        return Err(Box::new(Error::ParseError));
     }
 }
