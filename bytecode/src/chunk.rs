2828a3d88d0a16f47ebe9bd84f5531b08d0bfd5f
Karl Smeltzer <github@karlsmeltzer.com>
2025-05-17 20:31:10 -0700

    include line info for disassembly

--- bytecode/src/chunk.rs
+++ bytecode/src/chunk.rs
@@ -1,26 +1,35 @@
+use std::{collections::HashMap, ops::Range};
+
 use crate::{debug::disassemble_instructions, opcode::OpCode, value::Value};
 
+#[derive(Default)]
 pub struct Chunk {
     code: Vec<u8>,
     values: Vec<Value>,
+    lines: HashMap<u8, Range<u8>>,
 }
 
 impl Chunk {
     pub fn new() -> Self {
-        Chunk {
-            code: Vec::new(),
-            values: Vec::new(),
-        }
+        Chunk::default()
     }
 
-    pub fn write_op(&mut self, op: OpCode) -> u8 {
-        self.code.push(op.into());
-        (self.code.len() - 1) as u8
+    pub fn write_op(&mut self, op: OpCode, line: u8) -> u8 {
+        self.write_data(op.into(), line)
     }
 
-    pub fn write_data(&mut self, data: u8) -> u8 {
+    pub fn write_data(&mut self, data: u8, line: u8) -> u8 {
+        let code_last_idx = self.code.len() as u8;
         self.code.push(data);
-        (self.code.len() - 1) as u8
+
+        self.lines
+            .entry(line)
+            .and_modify(|range| range.end = code_last_idx + 1)
+            .or_insert(Range {
+                start: code_last_idx,
+                end: code_last_idx + 1,
+            });
+        code_last_idx
     }
 
     pub fn add_constant(&mut self, val: f64) -> u8 {
@@ -29,7 +38,9 @@ impl Chunk {
     }
 
     pub fn disassemble(&self, name: &str) {
+        println!("{:?}", &self.lines);
+
         println!("== {} ==", name);
-        disassemble_instructions(0, &self.code, &self.values);
+        disassemble_instructions(0, &self.code, &self.values, &self.lines);
     }
 }
--- bytecode/src/debug.rs
+++ bytecode/src/debug.rs
@@ -1,10 +1,19 @@
-use std::usize;
+use std::{collections::HashMap, ops::Range, usize};
 
 use crate::opcode::OpCode;
 
-pub fn disassemble_instructions(mut addr: usize, bytes: &[u8], values: &[f64]) {
+pub fn disassemble_instructions(
+    mut addr: usize,
+    bytes: &[u8],
+    values: &[f64],
+    lines: &HashMap<u8, Range<u8>>,
+) {
+    let mut line_for_addr = 0;
     while addr < bytes.len() {
         print!("{addr:0>4} ");
+
+        print_line_info(addr, lines, &mut line_for_addr);
+
         match TryInto::<OpCode>::try_into(bytes[addr]) {
             Ok(opcode) if is_constant_instruction(&opcode) => {
                 addr += disassemble_constant_instruction(&opcode, addr, bytes, values);
@@ -15,6 +24,16 @@ pub fn disassemble_instructions(mut addr: usize, bytes: &[u8], values: &[f64]) {
             Err(e) => panic!("{e}: Unable to parse opcode"),
         }
     }
+}
+
+fn print_line_info(addr: usize, lines: &HashMap<u8, Range<u8>>, line_for_addr: &mut u8) {
+    let line_for_next_addr = get_line_for_addr(addr, lines);
+    if *line_for_addr == line_for_next_addr {
+        print!("   | ");
+    } else {
+        print!("{line_for_next_addr:>4} ");
+    }
+    *line_for_addr = line_for_next_addr;
 }
 
 fn disassemble_simple_instruction(opcode: &OpCode) -> usize {
@@ -29,9 +48,10 @@ fn disassemble_constant_instruction(
     values: &[f64],
 ) -> usize {
     let constant_idx = bytes[addr + 1];
-    print!("{opcode:<16} ");
-    print!("{constant_idx:>4} ");
-    println!("{}", values[constant_idx as usize]);
+    println!(
+        "{opcode:<16} {constant_idx:>4} {}",
+        values[constant_idx as usize]
+    );
     2
 }
 
@@ -40,4 +60,15 @@ fn is_constant_instruction(opcode: &OpCode) -> bool {
         OpCode::Constant => true,
         _ => false,
     }
+}
+
+fn get_line_for_addr(addr: usize, lines: &HashMap<u8, Range<u8>>) -> u8 {
+    let addr_u8 = &(addr as u8);
+
+    for (line, range) in lines.iter() {
+        if range.contains(addr_u8) {
+            return *line;
+        }
+    }
+    return 0;
 }
